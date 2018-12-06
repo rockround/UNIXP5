4d9b12e06302e9405158d4f4062f40f482af52a2
Yuuki Zachary Ishimaru <yzi170030@cslinux1.utdallas.edu>
2018-12-06 12:26:56 -0600

    Added the rest of given random code, now must link gsl well

--- dealer.c
+++ dealer.c
@@ -1,14 +1,15 @@
-#include <gsl/rng.h>
+#include <gsl/gsl_rng.h>
 #include <stdio.h>
+#include <assert.h>
 int initTool();
 void finalizeTool();
 double getRDM();
 
 gsl_rng_type *_gsl_rng_type;
-gsl_rng *gsl_rng;
+gsl_rng *_gsl_rng;
 
 int initTool(){
-	_gsl_rng = gsl_rng_alloc(gsl_rng__mt19937);
+	_gsl_rng = gsl_rng_alloc(gsl_rng_mt19937);
 
 	if(_gsl_rng == NULL)
 		return 1;
@@ -23,4 +24,19 @@ void finalizeTool(){
 
 double getRDM(){
 	return(gsl_rng_uniform(_gsl_rng));
+}
+int main(){
+ int i;
+ 
+ const int NUM_ITERS=10;
+ 
+ assert(initTool()==0);
+ 
+ for(i=0;i<NUM_ITERS;i++)
+  printf("Random Number = %f\n",getRDM());
+ 
+ finalizeTool();
+ 
+ return 0;
+
 }
