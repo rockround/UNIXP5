0d591e95b1c5f4d163f3ee1246bd994a91d65338
Yuuki Zachary Ishimaru <yzi170030@cslinux1.utdallas.edu>
2018-12-07 20:23:58 -0600

    added hand.c

---
+++ hand.c
@@ -0,0 +1,54 @@
+#include <gsl/gsl_rng.h>
+#include <stdio.h>
+#include <assert.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+int initTool();
+void finalizeTool();
+int getRDM();
+
+gsl_rng_type *_gsl_rng_type;
+gsl_rng *_gsl_rng;
+
+int initTool(){
+	_gsl_rng = gsl_rng_alloc(gsl_rng_mt19937);
+
+	if(_gsl_rng == NULL)
+		return 1;
+	else
+		return 0;
+}
+
+void finalizeTool(){
+	gsl_rng_free(_gsl_rng);
+	return;
+}
+
+int getRDM(){
+	return(100*gsl_rng_uniform(_gsl_rng));
+}
+int main(int argc,char **argv ){
+ int i;
+ int option=0;
+ int percent = 0;
+ while((option=getopt(argc,argv,"p:"))!=-1){
+  switch(option){
+   case 'p':
+    percent = atoi(optarg);
+    break;
+   default:
+     printf("Invalid Syntax%d:\n",atoi(optarg));
+     return 1;
+    }
+  }
+ assert(initTool()==0);
+ if(getRDM()<=percent)
+  printf("Success");
+ else
+  printf("Fail");
+ finalizeTool();
+ 
+ return 0;
+
+}
--- makefile
+++ makefile
@@ -1,2 +1,4 @@
-make: dealer.c
+makeDealer: dealer.c
 	gcc $^ -o dealer -lgsl -L$$HOME/local/gsl-2.5/lib -lgslcblas -I$$HOME/local/gsl-2.5/include
+makeHand: hand.c
+	gcc $^ -o hand -lgsl -L$$HOME/local/gsl-2.5/lib -lgslcblas -I$$HOME/local/gsl-2.5/include
