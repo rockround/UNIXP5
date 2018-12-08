77bed92e45c8f30ac7b005ad87fa4915714176da
Yuuki Zachary Ishimaru <yzi170030@cslinux1.utdallas.edu>
2018-12-07 19:18:26 -0600

    Dealer finally accepts parameters

--- dealer.c
+++ dealer.c
@@ -1,6 +1,9 @@
 #include <gsl/gsl_rng.h>
 #include <stdio.h>
 #include <assert.h>
+#include <getopt.h>
+#include <stdlib.h>
+
 int initTool();
 void finalizeTool();
 double getRDM();
@@ -25,11 +28,35 @@ void finalizeTool(){
 double getRDM(){
 	return(gsl_rng_uniform(_gsl_rng));
 }
-int main(){
+int main(int argc,char **argv ){
  int i;
- 
+ int percent = 0;
+ int trials = 0;
+ int option = 0;
+ int verbose =0;
+ while((option=getopt(argc,argv,"v::p:"))!=-1){
+  switch(option){
+   case 'v':
+    verbose =1;
+    break;
+   case 'p':
+    percent = atoi(optarg);
+    break;
+   default:
+     printf("Invalid Syntax%d:\n",atoi(optarg));
+     return 1;
+    }
+  }
+  if(optind<argc){
+  // while(optind<argc)
+  //  printf("%s ",argv[optind++]);
+  // printf("\n");
+     trials = atoi(argv[optind]);
+   }else{
+    return 1;
+   }
  const int NUM_ITERS=10;
- 
+ printf("Percent:%d\t,Trials:%d\t,isVerbose:%d\n",percent,trials,verbose);
  assert(initTool()==0);
  
  for(i=0;i<NUM_ITERS;i++)
